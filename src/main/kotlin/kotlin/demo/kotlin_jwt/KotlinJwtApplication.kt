e8f32452b0ca6bd38ca07db5d1fbc367d671bebe
chahyunsoo <chahs1012@naver.com>
2024-06-25 02:31:01 +0900

    Init Project

---
+++ src/main/kotlin/kotlin/demo/kotlin_jwt/KotlinJwtApplication.kt
@@ -0,0 +1,11 @@
+package kotlin.demo.kotlin_jwt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinJwtApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinJwtApplication>(*args)
+}
---
+++ src/test/kotlin/kotlin/demo/kotlin_jwt/KotlinJwtApplicationTests.kt
@@ -0,0 +1,13 @@
+package kotlin.demo.kotlin_jwt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinJwtApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
